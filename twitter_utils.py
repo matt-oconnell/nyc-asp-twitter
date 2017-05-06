1b323ee6f6be0e498c434f99ee5bf9c8ec34e316
matt-oconnell <mattoconnell408@gmail.com>
2017-05-06 16:06:35 -0400

    parse time delta

--- twitter_utils.py
+++ twitter_utils.py
@@ -1,4 +1,6 @@
 import twitter
+from dateutil import parser
+from datetime import datetime
 import re
 import os
 
@@ -11,19 +13,26 @@ api = twitter.Api(consumer_key=os.environ.get('CONSUMER_KEY'),
                   access_token_secret=os.environ.get('ACCESS_TOKEN_SECRET')
                   )
 
+def getSuspendedDateDeltas(str):
+    # if not 'uspended' in str:
+    #     return []
 
-def getSuspendedDates(str):
-    if not 'uspended' in str:
-        return []
-    return re.findall(r'\w*.?\s\d\d*', str)
+    date_deltas = []
+    suspended_dates = re.findall(r'\w*.?\s\d\d*', str)
+
+    for date_str in suspended_dates:
+        delta = parser.parse(date_str) - datetime.now()
+        date_deltas.append(delta.days + 1)
+    return date_deltas
 
 
 def getParkingTweets():
     statuses = api.GetUserTimeline(screen_name='NYCASP')
     output = []
     for status in statuses:
+        suspended_dates = getSuspendedDateDeltas(status.text)
         output.append({
             'text': status.text,
-            'suspended_dates': getSuspendedDates(status.text)
+            'suspended_dates': suspended_dates
         })
     return output
